cf812d376585da11f1eeb550e9e7c00815cffd97
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-07-29 02:37:30 -0300

    Atualização do bootstrap

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,7 +3,7 @@ requirejs.config({
     paths: {
         "jquery": "//code.jquery.com/jquery-3.5.1.min",
         "popper": "//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min",
-        "bootstrap": "//stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min",
+        "bootstrap": "//cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min",
         "datatables": "//cdn.datatables.net/1.10.21/js/jquery.dataTables.min",
         "datatables.bootstrap": "//cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min",
         "datatables.responsive": "//cdn.datatables.net/responsive/2.2.5/js/dataTables.responsive.min",
